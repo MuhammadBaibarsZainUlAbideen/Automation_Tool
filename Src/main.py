159ca9ee3db0e4ca0dd2776c0b10cca20f2ce1e5
Baibars <baibarsjutt@gmail.com>
2026-02-05 10:07:17 -0600

    new

--- Src/main.py
+++ Src/main.py
@@ -6,7 +6,8 @@ import openai
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 reading = pd.read_excel("Files/Product Description.xlsx")
-stroing = reading["Prompt 1"]
+header_list = reading.columns.tolist()
+stroing = reading[header_list[2]]
 empty_list = []
 for i in range(0 ,len(reading)):
     response1 = openai.chat.completions.create(
@@ -25,3 +26,5 @@ for i in range(0 ,len(reading)):
 reading.to_excel("Ouput/o.xlsx",index=False)
 with pd.ExcelWriter("Files/Product Description.xlsx", engine="openpyxl", mode="a", if_sheet_exists="overlay") as writer:
     reading["Product Description (Result from Chat GPT)"].to_excel(writer,index=False,startcol=3)
+
+   
